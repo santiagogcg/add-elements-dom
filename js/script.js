9617c4f8d6a5f8c14b1afc0947662dc68fa44fa0
Santi <santiagogcg@gmail.com>
2024-02-23 21:53:35 +0100

    DOM sin corregir

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
  // Aquí tu código
+
+
+ 
+ 
+ let boton=document.getElementById("agregar");
+ let listadesordenada=document.getElementById("lista");
+ let lista1=document.createElement('li');
+ let lista2=document.createElement('li');
+ let lista3=document.createElement('li');
+
+ listadesordenada.appendChild(lista1);
+ listadesordenada.appendChild(lista2);
+ listadesordenada.appendChild(lista3);
+
+
+
+ boton.addEventListener("click",function funcion(){
+   
+    let edad=prompt("INGRESA EDAD");
+    lista1.innerHTML=edad;
+    let nombre=prompt("INGRESA NOMBRE");
+    lista2.innerHTML=nombre;
+    let genero=prompt("INGRESA GÉNERO");
+    lista3.innerHTML=genero;
+    
+
+ });
+   
